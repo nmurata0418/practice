2975aa56b980a226cc724d509f6e9ef83342a371
nmurata0418 <nmurata@skywill.jp>
2020-07-16 18:00:01 +0900

    本日分学習内容の追加

---
+++ vuecli_build/src/LikeNumber.vue
@@ -0,0 +1,22 @@
+<template>
+    <!--<p>いいね({{ number }})</p> ※Template内は一つのTagだけOK(div内に複数まとめればOK以下参照)-->
+    <div>
+        <p>いいね({{ number }})</p>
+        <button @click="increment">+1</button>
+    </div>
+</template>
+
+<script>
+    export default {
+    data: function() {
+        return {
+        number: 5
+        };
+    },
+    methods: {
+        increment() {
+            this.number += 1;
+        }
+    }
+};
+</script>
