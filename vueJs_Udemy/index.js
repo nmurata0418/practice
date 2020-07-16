71449b8543a2fd7daf810bba725487a785462a59
nmurata0418 <nmurata@skywill.jp>
2020-07-16 17:56:28 +0900

    本日分学習内容の追加

--- vueJs_Udemy/index.js
+++ vueJs_Udemy/index.js
@@ -5,15 +5,27 @@ new Vue({
     },
 });
 
-Vue.component('my-component', {
+let component = {
     data: function() {
         return {
             number: 12
         }
     },
-    template: '<p>いいね({{number}})</p>'
-})
+    template: '<p>いいね({{number}})<button @click="increment">+1</button></p>',
+    methods: {
+        increment: function() {
+            this.number += 1;
+        }
+    }
+}
 
 new Vue({
     el: '#app-2',
+})
+
+new Vue({
+    el: '#app-3',
+    components: {
+        'my-component': component
+    }
 })
