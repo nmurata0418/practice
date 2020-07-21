6511a5e321780bb56ab77479b21849c3350c91b2
nmurata0418 <nmurata@skywill.jp>
2020-07-21 17:32:30 +0900

    7月21日分追加

---
+++ vuecli_rebuild/src/components/LikeHeader.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <slot name="title" :user="user" :text="text" text2="テキスト" :aiueo="test">デフォルトのタイトル</slot>
+        <slot></slot>
+        <slot name="favorite"></slot>
+        <hr>
+        <p>いいねの数</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            user: {
+                firstName: "Jack",
+                lastName: "Donald"
+            },
+            text: {
+                text: "あいうえお"
+            }
+        };
+    }
+};
+</script>
+
+<style scoped>
+div {
+    border: 1px solid blue;
+}
+h1 {
+    color: red;
+}
+</style>
