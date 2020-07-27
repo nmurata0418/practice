2f2fb970962bb0b73e52323632d536e1220ec550
nmurata <nmurata@skywill.jp>
2020-07-27 17:43:54 +0900

    7月27日分更新

--- vuecli_Form/src/main.js
+++ vuecli_Form/src/main.js
@@ -4,6 +4,15 @@ import LikeNumber from "./components/LikeNumber.vue";
 
 Vue.config.productionTip = false;
 Vue.component("LikeNumber", LikeNumber);
+Vue.directive("border", function () {
+});
+//{
+// bind(el, binding, vnode) {}, //重要
+// inserted(el, binding, vnode) {},
+// update(el, binding, vnode, oldvnode) {}, //重要
+// componentUpdated(el, binding, vnode, oldvnode) {},
+// unbind(el, binding, vnode) {},
+// });
 
 new Vue({
   render: h => h(App)
