33ec1e719260e0602e056d18d14fae628825e47b
nmurata <nmurata@skywill.jp>
2020-07-31 18:01:55 +0900

    commit

---
+++ vuecli_spa/src/views/Users.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h3>Users</h3>
+        <button @click="toHome">Homeのページに行く</button>
+        <hr>
+        <h1>User No. {{ $route.params.id }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        toHome() {
+            this.$router.push('/');
+        }
+    }
+}
+</script>
