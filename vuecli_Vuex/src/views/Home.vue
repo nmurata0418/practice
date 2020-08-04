d4b452e411ceee4ea6d94bb89883b5541bf190c9
nmurata0418 <nmurata@skywill.jp>
2020-08-04 17:59:41 +0900

    8月4日分追加

---
+++ vuecli_Vuex/src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h3>Home</h3>
+    <button @click="toUsers">Usersのページに行く</button>
+    <p>{{ doubleCount }}</p>
+    <p>{{ tripleCount }}</p>
+    <input
+    type="text"
+    :value="message"
+    @input="updateMessage">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(["doubleCount", "tripleCount"]),
+    message() {
+      return this.$store.getters.message;
+    }
+  },
+  // {
+  //   doubleCount() {
+  //     return this.$store.getters.doubleCount;
+  //   },
+  //   tripleCount() {
+  //     return this.$store.getters.tripleCount;
+  //   }
+  // },
+  methods: {
+    updateMessage(e) {
+      this.$store.dispatch("updateMessage", e.target.value);
+    },
+    toUsers() {
+      this.$router.push({
+        name: "users-id-profile",
+        params: { id: 1 }
+      });
+    }
+  }
+};
+</script>
