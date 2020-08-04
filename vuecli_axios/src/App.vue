726bc6df7c237fb2300a9a9973eb891c57cd113e
nmurata0418 <nmurata@skywill.jp>
2020-08-04 18:08:33 +0900

    7月30日分追加

---
+++ vuecli_axios/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <h3>掲示板に投稿する</h3>
+    <label for="name">ニックネーム</label>
+    <input
+    type="text"
+    id="name"
+    v-model="name"
+    >
+    <br><br>
+    <label for="comment">コメント：</label>
+    <textarea
+    id="comment"
+    v-model="name"
+    ></textarea>
+    <h2>掲示板</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      comment: "",
+    };
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
