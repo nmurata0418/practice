cdca34e6a153587a230a68125ae14e8a67b56649
nmurata0418 <nmurata@skywill.com>
2020-08-05 18:34:04 +0900

    8月5日分

---
+++ vuecli_login/src/views/Login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h2>ログイン</h2>
+        <label for="email">Email：</label>
+        <input
+            id="email"
+            type="email"
+            v-model="email"
+        >
+        <br><br>
+        <label for="password">パスワード：</label>
+        <input
+            id="password"
+            type="password"
+            v-model="password"
+        >
+
+        <br><br>
+        <button @click="login">送信</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+        name: '',
+        password: '',
+        };
+    },
+    methods: {
+        login() {}
+    }
+};
+</script>
+
---
+++ vuecli_login/src/views/Register.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h2>登録</h2>
+        <label for="email">Email：</label>
+        <input
+            id="email"
+            type="email"
+            v-model="email"
+        >
+        <br><br>
+        <label for="password">パスワード：</label>
+        <input
+            id="password"
+            type="password"
+            v-model="password"
+        >
+
+        <br><br>
+        <button @click="register">送信</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+        name: '',
+        register: '',
+        };
+    },
+    methods: {
+        login() {}
+    }
+};
+</script>
