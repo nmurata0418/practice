e8109572abd043b606030d868ac255159779a8b8
nmurata0418 <nmurata@skywill.com>
2020-08-07 15:54:34 +0900

    8月7日分追加

---
+++ nuxt_project/nuxt-assets/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div>
+      <img src="~/assets/cat.jpg">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+img {
+  width: 100px;
+}
+</style>
---
+++ nuxt_project/nuxt-view/layouts/error.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1 -if="error.statusCode === 404">Error! 404</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['error']
+}
+</script>
